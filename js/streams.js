b4217374232be6943b70f9c8310ca3fcbc675b12
Jason Waryold <71293360+jwaryold2@users.noreply.github.com>
2023-02-15 18:22:50 -0500

    w

---
+++ js/streams.js
@@ -0,0 +1,6 @@
+const btn = document.getElementsById('btn');
+btn.addEventListener('click', function master() {
+    const inputval = document.getElementsById('textb');
+    return inputval;
+})
+console.log(inputval);
